56a5578c22148f0a05476383acbcd465c28f04e3
gloomforge <softly2200@gmail.com>
2025-06-22 12:50:17 +0300

    feat: add basic pages for routing

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,8 @@
+export default function LoginPage() {
+  return (
+    <div>
+      <h1>Войти</h1>
+      <p>Чтобы войти на сайт введите ваш email и пароль</p>
+    </div>
+  );
+}
---
+++ src/pages/Register/index.jsx
@@ -0,0 +1,8 @@
+export default function RegisterPage() {
+  return (
+    <div>
+      <h1>Регистрация</h1>
+      <p>Чтобы войти на сайт введите ваш email и пароль</p>
+    </div>
+  );
+}
