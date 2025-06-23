ad8f3e5799e03fe5373835db46be2954e4879165
gloomforge <softly2200@gmail.com>
2025-06-23 08:54:20 +0300

    perf: optimize login and register page

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,17 +1,17 @@
 import { Link } from 'react-router';
-import { LoginForm } from '../../features/auth';
+import { LogForm } from '../../features/auth';
 import styles from '../../styles/AuthPage.module.css';
 import { ROUTES } from '../../config';
 
 export default function LoginPage() {
   return (
-    <div className={styles['wrapper']}>
+    <div className={styles.wrapper}>
       <div className={styles['container']}>
         <h1 className={styles['title']}>Войти</h1>
         <p className={styles['subtitle']}>
           Чтобы войти на сайт введите ваш email и пароль
         </p>
-        <LoginForm />
+        <LogForm />
 
         <p className={styles['login-text']}>
           Еще нету аккаунта? <Link to={ROUTES.REGISTER}>Создать аккаунт</Link>
--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,17 +1,17 @@
 import { Link } from 'react-router';
-import { RegisterForm } from '../../features/auth';
+import { RegForm } from '../../features/auth';
 import styles from '../../styles/AuthPage.module.css';
 import { ROUTES } from '../../config';
 
 export default function RegisterPage() {
   return (
-    <div className={styles['wrapper']}>
+    <div className={styles.wrapper}>
       <div className={styles['container']}>
         <h1 className={styles['title']}>Регистрация</h1>
         <p className={styles['subtitle']}>
           Чтобы войти на сайт введите ваш email и пароль
         </p>
-        <RegisterForm />
+        <RegForm />
 
         <p className={styles['login-text']}>
           Уже есть аккаунт? <Link to={ROUTES.LOGIN}>Войти</Link>
