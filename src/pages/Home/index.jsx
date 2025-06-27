b5add7cf7ceeaf40dcf36d12dba82a5a76bb0bef
gloomforge <softly2200@gmail.com>
2025-06-27 10:04:52 +0300

    feat: add notification to home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,34 @@
 import styles from '../../styles/HomePage.module.css';
 import iphone from '../../assets/image/iphone-image.png';
 import Category from '../../components/Category';
+import { useLocation } from 'react-router';
+import { useEffect, useState } from 'react';
+import Notification from '../../components/Notification';
 
 const HomePage = () => {
+  const location = useLocation();
+  const [showNotification, setShowNotification] = useState(false);
+
+  useEffect(() => {
+    if (location.state?.fromAuth) {
+      setShowNotification(true);
+      window.history.replaceState({}, document.title);
+    }
+  }, [location.state]);
+
   const handleShopClick = () => {
     window.location.href = '/shop';
   };
 
   return (
     <div className={styles.wrapper}>
+      {showNotification && (
+        <Notification
+          title="Вы уже вошли в аккаунт"
+          description="Если хотите, вы можете выйти из аккаунта."
+          onClose={() => setShowNotification(false)}
+        />
+      )}
       <section className={styles.hero}>
         <div className={styles.heroContent}>
           <h1>Откройте будущее с техникой Apple по доступным ценам!</h1>
@@ -19,7 +39,6 @@ const HomePage = () => {
           <img src={iphone} alt="iPhone" className={styles.heroImage} />
         </div>
       </section>
-
       <Category />
     </div>
   );
